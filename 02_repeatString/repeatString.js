b1e735a76c696e3a1f4b7ff64dd94a8bfe2b3afe
Marc Wolpert <mewolpert@gmail.com>
2024-09-26 16:50:32 -0700

    test: Passed all string test for arbitrary concats

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(baseString, count) {
+    if (count<0){
+        return "ERROR"
+    }
+    superString=""
+    for (let i=0; i<count; i++){
+        superString+=baseString;
+    }
+    return superString;
 };
 
 // Do not edit below this line
