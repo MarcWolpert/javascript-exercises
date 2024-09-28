8ced79b20cca849c03ff64764ca02b93c89e13f2
Marc Wolpert <mewolpert@gmail.com>
2024-09-27 17:39:15 -0700

    test: All tests passed to sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(lowerBound,upperBound) {
+    if (lowerBound>=upperBound){
+        let temp=upperBound;
+        upperBound=lowerBound;
+        lowerBound=temp;
+    }
+    //Guaranteeing both non-negative
+    if (lowerBound<0 || upperBound<0){
+        return "ERROR";
+    }
+    //Guaranteeing Number
+    if ( (typeof lowerBound)!=='number' || (typeof upperBound)!=='number'){
+        return "ERROR";
+    }
+    //Guaranteeing Integer
+    if (!Number.isInteger(lowerBound) || !(Number.isInteger(upperBound))){
+        return "ERROR";
+    }
+    let sum=0;
+    for (let i=lowerBound; i<=upperBound; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
