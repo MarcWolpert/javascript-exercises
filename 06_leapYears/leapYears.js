332046ec83eff802ec30162150ab74b05b06a7d8
Marc Wolpert <mewolpert@gmail.com>
2024-09-27 17:59:42 -0700

    test: Passed all leap year tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisible4=year%4===0;
+    const isDivisible400=year%400===0;
+    const isDivisible100=year%100===0;
 
+    if (!isDivisible4){ 
+        return false;
+    }
+    if (isDivisible100 && !isDivisible400){
+        return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
