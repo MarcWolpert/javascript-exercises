fbdc0fbd3623b19e6f0f7cb538cf50e9928b16c4
Marc Wolpert <mewolpert@gmail.com>
2024-09-27 18:10:02 -0700

    test: All tests passed for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  //C=(F-32)*5/9
+  const result=Number(((temperature-32)*(5/9)).toFixed(1));
+  return (result===0.0) ? 0: result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  //F=(9/5) C + 32
+  const result=Number(((9/5)*temperature+32).toFixed(1));
+  return (result===0.0) ? 0: result;
 };
 
 // Do not edit below this line
