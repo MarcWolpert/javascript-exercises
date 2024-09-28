b0ad9fa6876d464c9ea6a1aa87f503e97baa81ee
Marc Wolpert <mewolpert@gmail.com>
2024-09-27 17:17:43 -0700

    test: Passed all tests removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let arr=arguments[0];
+    for (let i=1; i<arguments.length; i++){
+        //search for items in array and removes all
+        //occurrences in that array
+        arr=arr.filter( (arg) => arg!==arguments[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
