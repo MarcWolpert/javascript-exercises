fbdb10a35f7123790179c1cdfb1f5eec3cbdd4c5
Marc Wolpert <mewolpert@gmail.com>
2024-10-19 18:29:47 -0700

    test: All tests passed for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (typeof input !== "number"){
+        input=Number(input);
+    };
+    let prev=1;
+    let curr=1;
+    if ( (input===1) || (input===2) ){return 1};
+    if ( (input===0)) { return 0;};
+    if ( (input<0)) { return "OOPS";};
 
+    for(let i=3; i<=input; i++){
+        let oldCurr=curr;
+        curr=prev+curr;
+        prev=oldCurr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
