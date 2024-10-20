299e91f5e31f6d473d6eb477b426dee1f4ad47f9
Marc Wolpert <mewolpert@gmail.com>
2024-10-19 19:20:48 -0700

    test: All tests passed for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let eldest={};
+    let eldestAge=0;
+
+    //filter out without yearofdeath
+    const withoutYearOfDeath=people.filter((person)=>{
+        return person.yearOfDeath===undefined;
+    })
+    people.map( (person)=>{
+        if ('yearOfDeath' in person){
+            finalAge=person.yearOfDeath-person.yearOfBirth;
+            if (finalAge>eldestAge){ 
+                eldestAge=finalAge;
+                eldest=person;
+            }
+        }
+    })
+    //if year of death is undefined select the one with the oldest
+    //date of birth compared with the eldest so far
+    withoutYearOfDeath.map((person)=>{
+        if (person.yearOfBirth<eldest.yearOfBirth){
+            eldest=person;
+        }
+    })
+    return eldest;
 
 };
 
