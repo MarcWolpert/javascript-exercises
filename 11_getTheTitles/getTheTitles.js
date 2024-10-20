200c2c117638299f8b4c467ce7b40ddb69c6c99a
Marc Wolpert <mewolpert@gmail.com>
2024-10-19 18:38:38 -0700

    test: Passed all tests for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(associativeArr) {
+    arr=[]
+    associativeArr.map((item)=>arr.push(item.title));
+    return arr;
 };
 
 // Do not edit below this line
