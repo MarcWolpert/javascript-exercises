bef40752ec17b0587edc33883c3cefe93dc9af1b
Marc Wolpert <mewolpert@gmail.com>
2024-10-19 18:19:12 -0700

    test: Passed all calculator and palindrome tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(addend1,addend2) {
+	return addend1+addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend,subtrahend) {
+	return minuend-subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,adder)=>{
+    return acc+adder;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc,multiplier)=>{
+    return acc*multiplier
+  },1);
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total=1;
+  for (let i=num; i>0; i--){
+    total*=i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let arr=[...input];
+    console.log(arr);
+    let arr_filtered=arr.filter((char)=>{
+        return /[a-zA-Z0-9]/.test(char);
+    })
+    console.log(arr_filtered);
+    const forward_nopunc=arr_filtered.toString().replaceAll(',',"").toLowerCase();
+    const reverse_nopunc=[...forward_nopunc].reverse().toString().replaceAll(',',"").toLowerCase();
+    console.log(`f:${forward_nopunc} \nb:${reverse_nopunc}`);
+    return forward_nopunc===reverse_nopunc;
 };
 
 // Do not edit below this line
